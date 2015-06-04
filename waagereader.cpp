aa206e9322af401de408c6c8847ac3d7c06f09e1
phiresky <phiresky@users.noreply.github.com>
2015-06-04 16:04:03 +0200

    add output when reading from file for debug

--- waagereader.cpp
+++ waagereader.cpp
@@ -12,18 +12,24 @@ using byte = unsigned char;
 uint64_t freq = 44100; // target read frequency
 // minimum pulse width to interpret bit as 1 (25 samples)
 int yeslen_us = 560; 
-int controllen_us = 900; // minimum control bit length (40 samples)
+int min_controllen_us = 900; // minimum control bit length (40 samples)
+int max_controllen_us = 1800; // maximum control bit length (80 samples)
 uint64_t iteration_us = 1500000;
 uint64_t capture_us =   100000;
 
+bool voice = true;
+bool outputFiltered = false;
+
 void weight_callback(double weight) {
+	if(!voice) return;
 	char buf[100];
 	snprintf(buf,100,"say %.1f kilogram &", weight);
 	system(buf);
 }
 
 int yes_samples = yeslen_us*freq/1000000;
-int control_samples = controllen_us*freq/1000000;
+int min_control_samples = min_controllen_us*freq/1000000;
+int max_control_samples = max_controllen_us*freq/1000000;
 
 void busy_wait(timeval* time, timeval* delay) {
 	timeradd(time, delay, time);
@@ -56,8 +62,8 @@ uint64_t tobytes(vector<byte> bits, int start, int len) {
 	return out;
 }
 
-void parsepacket(vector<byte> bits) {
-	if(bits.size() < 5*8) return;
+bool parsepacket(vector<byte> bits) {
+	if(bits.size() != 6*8) return false;
 	uint64_t type = tobytes(bits, 0, 3*8+5) << 3;
 	if(type == 0x00100020) {
 		double weight = tobytes(bits, 3*8+4, 12)/10.0;
@@ -69,40 +75,56 @@ void parsepacket(vector<byte> bits) {
 	cerr << "(";
 	for(byte c:bits) cerr << (c?'1':'0');
 	cerr << ")\n";
+	return true;
 }
 
 
 vector<byte> current_bits;
-void next_pulse(bool high, int length) {
-	static bool running = false;
+bool running = false;
+bool next_pulse(bool high, int length) {
 	if(!high) {
-		if(length > yes_samples) {
-			parsepacket(current_bits);
+		if(running && length > yes_samples) {
+			bool valid = parsepacket(current_bits);
 			running = false;
 			current_bits.clear();
+			return valid;
 		}
 	} else {
-		if(length > control_samples) {
+		if(length > min_control_samples) {
 			if(running) {
-				parsepacket(current_bits);
+				// parsepacket(current_bits);
 				current_bits.clear();
-			} else running = true;
+			}
+			running = length < max_control_samples;
 		} else if(running) {
 			current_bits.push_back(length < yes_samples ? 0 : 1);
 		}
 	}
+	return false;
 }
 void analyze(byte* cache, int cache_size) {
 	bool state = false;
 	int curval = 0;
 	int length = 0;
+	int lastPacketStart = 0;
 	for(int i=0;i<cache_size;i++) {
 		// ignore flips shorter than ~2 samples
-		curval = int((cache[i]+curval)/2); 
+		curval = int((cache[i]+3*curval)/4); 
 		if((curval>127) != state) {
-			next_pulse(state, length);
+			bool running_before = running;
+			bool received = next_pulse(state, length);
+			if(!running_before && running && outputFiltered) {
+				for(;lastPacketStart < i;lastPacketStart++) {
+					putchar(127);
+				}
+			}
+			if(received && outputFiltered) {
+				for(;lastPacketStart < i - length; lastPacketStart++) {
+					putchar(cache[lastPacketStart]);
+				}
+			}
 			state = curval > 127;
-			length = 0;
+			length = 1;
 		} else length++;
 	}
 	
@@ -121,6 +143,8 @@ pair<byte*,int> readfile(string name) {
 
 int main(int argc, char* argv[]) {
 	if(argc>1) {
+		voice = false;
+		outputFiltered = true;
 		auto x = readfile(argv[1]);
 		analyze(x.first,x.second);
 		return 0;
